7a57d05307ef476dce2bb48619b2ab47aa05e9d7
AkishinoShiame <akishinoshiame@icloud.com>
2019-12-14 23:59:52 +0800

    update sphinx and test for gitpage

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = 'アルファ'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = [ 'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -60,3 +60,13 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+#-- Support for CommonMarkParser ---------------------------------------------
+
+# this section is for adding the support in Markdown files.
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {'.md': CommonMarkParser}
+source_suffix = ['.rst', '.md']
+
