bf293d5ffe25cdfe28d179ff3f4d90ddd59f14fb
AkishinoShiame <akishinoshiame@icloud.com>
2021-07-12 00:26:40 +0900

    sphinx文書を使う、サイト作成。

--- StoryLine.Main/pyweb/source/conf.py
+++ StoryLine.Main/pyweb/source/conf.py
@@ -30,7 +30,9 @@ release = 'v0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx.ext.githubpages'
+extensions = [
+    'sphinx.ext.githubpages',
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
